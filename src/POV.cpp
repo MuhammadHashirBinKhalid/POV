e9e045f1ee6f8863bf19e6da1772ab618323ef11
MuhammadHashirBinKhalid <mohammadhashirbinkhalid@gmail.com>
2019-04-07 18:30:59 +0500

    Basic class structure

---
+++ src/POV.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// Our own Header File
+#include <POV.h>
+
+
+POV::POV(){
+//constructor 
+}
+
+
+bool POV::init(){
+//1st methond
+}
+
---
+++ src/POV.h
@@ -0,0 +1,31 @@
+/***************************************************
+Copyrights 2019
+Muhammad Hashir Bin Khalid (github: MuhammadHashirBinKhalid)
+***************************************************/
+
+// Ensuring Library Description is only included once
+#ifndef POV_H_
+#define POV_H_
+
+// #include <stdint.h>
+
+// Directives
+
+// Variables
+
+// Constants
+
+// Classes
+class POV {
+
+public:
+    explicit POV();
+    void begin();
+
+protected:
+
+private:
+
+};
+
+#endif  // ARDUINOML_H_
