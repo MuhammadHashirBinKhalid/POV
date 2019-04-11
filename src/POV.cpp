88ea1617835c8b69dab4d697c8348e35fbe227c9
Muhammad Hashir Bin Khalid - <mohammadhashirbinkhalid@gmail.com>
2019-04-11 15:45:32 +0500

    Compiled code. Not working fine. But compiles fine.

--- src/POV.cpp
+++ src/POV.cpp
@@ -1,89 +1,84 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <FastLED.h>
-
 // Our own Header File
 #include <POV.h>
 
 //Constructor
 POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1){
-_num_leds=number_of_led;
+_num_leds=number_of_led_per_strip;
 _num_strips=1;
 }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1,uint8_t d2,uint8_t c2){
-_num_leds=number_of_led;
-_num_strips=2;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1,uint8_t d2,uint8_t c2){
+// _num_leds=number_of_led;
+// _num_strips=2;
+// }
 
-POV::POV(int number_of_led_per_strip, ,uint8_t d1,uint8_t c1,uint8_t d2,uint8_t c2,uint8_t d3,uint8_t c3){
-_num_leds=number_of_led;
-_num_strips=3;
-}
+// POV::POV(int number_of_led_per_strip, ,uint8_t d1,uint8_t c1,uint8_t d2,uint8_t c2,uint8_t d3,uint8_t c3){
+// _num_leds=number_of_led;
+// _num_strips=3;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4){
-_num_leds=number_of_led;
-_num_strips=4;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4){
+// _num_leds=number_of_led;
+// _num_strips=4;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5){
-_num_leds=number_of_led;
-_num_strips=5;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5){
+// _num_leds=number_of_led;
+// _num_strips=5;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6){
-_num_leds=number_of_led;
-_num_strips=6;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6){
+// _num_leds=number_of_led;
+// _num_strips=6;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7){
-_num_leds=number_of_led;
-_num_strips=7;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7){
+// _num_leds=number_of_led;
+// _num_strips=7;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
-, uint8_t d8,uint8_t c8){
-_num_leds=number_of_led;
-_num_strips=8;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
+// , uint8_t d8,uint8_t c8){
+// _num_leds=number_of_led;
+// _num_strips=8;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
-, uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9){
-_num_leds=number_of_led;
-_num_strips=9;
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
+// , uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9){
+// _num_leds=number_of_led;
+// _num_strips=9;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
-, uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9, uint8_t d10,uint8_t c10){
-_num_leds=number_of_led;
-_num_strips=10;
-_pins[_num_strips][2]={{d1,c1},{d2,c2},{d3,c3},{d4,c4},{d5,c5},{d6,c6},{d7,c7},{d8,c8},{d9,c9},{d10,c10}}
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
+// , uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9, uint8_t d10,uint8_t c10){
+// _num_leds=number_of_led;
+// _num_strips=10;
+// }
 
-POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
-, uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
-, uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9, uint8_t d10,uint8_t c10, uint8_t d11,uint8_t c11){
-_num_leds=number_of_led;
-_num_strips=11;
-_pins[_num_strips][2]={{d1,c1},{d2,c2},{d3,c3},{d4,c4},{d5,c5},{d6,c6},{d7,c7},{d8,c8},{d9,c9},{d10,c10}
-,{d11,c11}}
-}
+// POV::POV(int number_of_led_per_strip, uint8_t d1,uint8_t c1, uint8_t d2,uint8_t c2, uint8_t d3,uint8_t c3
+// , uint8_t d4,uint8_t c4, uint8_t d5,uint8_t c5, uint8_t d6,uint8_t c6, uint8_t d7,uint8_t c7
+// , uint8_t d8,uint8_t c8, uint8_t d9,uint8_t c9, uint8_t d10,uint8_t c10, uint8_t d11,uint8_t c11){
+// _num_leds=number_of_led;
+// _num_strips=11;
+// }
 
-bool POV::init(int sensorpin){
+void POV::init(int sensorpin){
 //1st methond
 _sensorpin=sensorpin;
-CRGB leds[_num_strips][_num_leds]; 
-for(int i=0;i<_num_strips,i++)
- { FastLED.addLeds<APA102, _pins[i][1], _pins[i][2]>(leds1, _num_leds);}
+ 
+int _pins[1][2]={{3,4}};
+//for(int i=0;i<_num_strips,i++)
+ //{ 
+   FastLED.addLeds<APA102, D1, D2>(leds, 8);
+   //}
   setBrightness(BRIGHTNESS);
-
 }
 
 void POV::setBrightness(int brightness){
@@ -104,7 +99,7 @@ if (_State == HIGH) {
   }
   _current_micros = micros();
 
- if (_current_micros - _previous_micros4 >= T) {
+ if (_current_micros - _previous_micros4 >= _T) {
     if (!_b2init) {
       _i2 = 0;
       _m2 = 0;
@@ -120,7 +115,7 @@ if (_State == HIGH) {
   }
 
   if ((_current_micros - _previous_micros1 >= _dottime) && (_m1 < 48) && (_i1 < 23)) {
-    printLetterb1(_alpha[toprint[_i1]]);
+    printLetterb1(_alpha[_toprint[_i1]]);
     if (_m1 >= 48) {
       _i1++;
       _m1 = 0;
@@ -128,25 +123,39 @@ if (_State == HIGH) {
     _previous_micros1 = micros();
   }
 
-  if ((_current_micros - _previous_micros2 >= _dottime) && (_m2 < 48) && (_i2 < 23)) {
-    printLetterb2(_alpha[toprint[_i2]]);
-    if (_m2 >= 48) {
-      _i2++;
-      _m2 = 0;
-    }
-    _previous_micros2 = micros();
-  }
+  // if ((_current_micros - _previous_micros2 >= _dottime) && (_m2 < 48) && (_i2 < 23)) {
+  //   printLetterb2(_alpha[toprint[_i2]]);
+  //   if (_m2 >= 48) {
+  //     _i2++;
+  //     _m2 = 0;
+  //   }
+  //   _previous_micros2 = micros();
+  // }
 
-  if ((current_micros - previous_micros3 >= dottime) && (m3 < 48) && (i3 < 23)) {
-    printLetterb3(alpha[toprint[i3]]);
-    if (m3 >= 48) {
-      i3++;
-      m3 = 0;
-    }
-    previous_micros3 = micros();
-  }
+  // if ((_current_micros - _previous_micros3 >= dottime) && (_m3 < 48) && (_i3 < 23)) {
+  //   printLetterb3(_alpha[toprint[_i3]]);
+  //   if (_m3 >= 48) {
+  //     _i3++;
+  //     _m3 = 0;
+  //   }
+  //   _previous_micros3 = micros();
+  // }
 }
 
 void POV::setState(){
     _State=HIGH;
+}
+
+void POV::printLetterb1(bool letter[])
+{
+  for (int Pin = 0; Pin <= _num_leds; Pin++) {
+    if (letter[_m1] == 1) {
+      leds[Pin] = CRGB::Green;
+    }
+    else {
+      leds[Pin] = CRGB::Black;
+    }
+    _m1++;
+  }
+  FastLED.show();
 }
