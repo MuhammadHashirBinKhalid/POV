9bc2554a49dddef282f6e7ad18b0adbbfeba091f
Urooj Alam <urooj-alam@outlook.com>
2019-04-28 22:29:33 +0500

    Update POV_basic.ino

--- examples/POV_basic/POV_basic.ino
+++ examples/POV_basic/POV_basic.ino
@@ -1,91 +1,18 @@
 #include <ESP8266WiFi.h>
-#include "textdisplay.h"
 #include <FastLED.h>
-#define NUM_LEDS 8
-#define DATA_PIN D1 //for blade1
-#define CLOCK_PIN D2 //for blade1
-#define DATA_PIN1 D5 //for blade2
-#define CLOCK_PIN1 D6 //for blade2
-#define DATA_PIN2 D3 //for blade3 
-#define CLOCK_PIN2 D8 //for blade3
-//#define color 
-CRGB leds1[NUM_LEDS];
-CRGB leds2[NUM_LEDS];
-CRGB leds3[NUM_LEDS];
-const char* ssid = "VPN IC";
-const char* password = "abcd1234";
-String value;   //string for fontsize
-char charBuf[60];
-String value1; //string for text
-// Create an instance of the server
-// specify the port to listen on as an argument
-WiFiServer server(80);
+#include <POV.h>
+POV pov(8, D1,D2,D5,D6,D3,D8);
 
-bool alpha[37][48] = {
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //0
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //1
-  {1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //2
-  {1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //3
-  {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //4
-  {1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //5
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //6
-  {1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //7
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //8
-  {1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //9
+const char* ssid = "";
+const char* password = "";
 
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //A=10
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //B=11
-  {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //C=12
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //D=13
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //E=14
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //F=15
-  {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //G=16
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //H=17
-  {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //I=18
-  {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //J=19
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //K=20
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //L=21
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //M=22
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //N=23
-  {0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //O=24
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //P=25
-  {0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //Q=26
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //R=27
-  {0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //S=28
-  {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //T=29
-  {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //U=30
-  {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //V=31
-  {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //W=32
-  {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //X=33
-  {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //Y=34
-  {1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, //Z=35
-
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} //Empty = 36
-};
-
-int toprint[22] = {17, 14, 21, 21, 24, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36};
-
-//#define BTW_BLADES 73000
-int fontsize = 2000;
-int max_dig = 23;
-int dottime;
-int ivalue = 0;
-int sensor = D7;
-int State = 0;
-int lastState = 0;
-bool mybool = 0;
-unsigned long current_micros;
-unsigned long previous_micros1 = 0, previous_micros2 = 0, previous_micros3 = 0, previous_micros4 = 0;
-int m1 = 0, m2 = 0, m3 = 0;
-bool b1init = 0, b2init = 0, b3init = 0;
 
 void setup() {
-  attachInterrupt(digitalPinToInterrupt(sensor), ISR, CHANGE);
-  FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds1, NUM_LEDS);
-  FastLED.addLeds<APA102, DATA_PIN1, CLOCK_PIN1>(leds2, NUM_LEDS);
-  FastLED.addLeds<APA102, DATA_PIN2, CLOCK_PIN2>(leds3, NUM_LEDS);
-  FastLED.setBrightness(60);
+  //attachInterrupt(digitalPinToInterrupt(sensor), ISR, CHANGE);
+
   Serial.begin(115200);
+    pov.init(D7);
+    pov.setState();
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -100,141 +27,22 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
 
   // Print the IP address
   Serial.println(WiFi.localIP());
 }
 
-int i1 = 0, i2 = 0, i3 = 0;
-unsigned long T;
-int tempcount = 0;
-int tempvalue=0;
 void loop() {
-  // Check if a client has connected
- 
- povserver();
-  
-   if (State == HIGH) {
-    previous_micros1 = micros();
-    T = (previous_micros1 - previous_micros4) / 3.0;
-    dottime = T / 73000.0 * fontsize;
-    
-    previous_micros4 = micros();
-    m1 = 0;
-    i1 = 0;
-    b1init = 0; b2init = 0; b3init = 0;
-    State = LOW;
-  }
 
-
-  current_micros = micros();
-
-  if (current_micros - previous_micros4 >= T) {
-    if (!b2init) {
-      i2 = 0;
-      m2 = 0;
-      b2init = 1;
-    }
-  }
-  if (current_micros - previous_micros4 >= (2 * T)) {
-    if (!b3init) {
-      i3 = 0;
-      m3 = 0;
-      b3init = 1;
-    }
-  }
-
-  if ((current_micros - previous_micros1 >= dottime) && (m1 < 48) && (i1 < 23)) {
-    printLetterb1(alpha[toprint[i1]]);
-    if (m1 >= 48) {
-      i1++;
-      m1 = 0;
-    }
-    previous_micros1 = micros();
-  }
-
-  if ((current_micros - previous_micros2 >= dottime) && (m2 < 48) && (i2 < 23)) {
-    printLetterb2(alpha[toprint[i2]]);
-    if (m2 >= 48) {
-      i2++;
-      m2 = 0;
-    }
-    previous_micros2 = micros();
-  }
-
-  if ((current_micros - previous_micros3 >= dottime) && (m3 < 48) && (i3 < 23)) {
-    printLetterb3(alpha[toprint[i3]]);
-    if (m3 >= 48) {
-      i3++;
-      m3 = 0;
-    }
-    previous_micros3 = micros();
-  }
-  lastState = State;
+pov.server();
+pov.run();
 }
 
-void printLetterb1(bool letter[])
-{
-  for (int Pin = 0; Pin <= 7; Pin++) {
-    if (letter[m1] == 1) {
-      leds1[Pin] = CRGB::Green;
-    }
-    else {
-      leds1[Pin] = CRGB::Black;
-    }
-    m1++;
-  }
-  FastLED.show();
-}
-
-void printLetterb2(bool letter[])
-{
-  for (int Pin = 0; Pin <= 7; Pin++) {
-    if (letter[m2] == 1) {
-      leds2[Pin] = CRGB::Green;
-    }
-    else {
-      leds2[Pin] = CRGB::Black;
-    }
-    m2++;
-  }
-  FastLED.show();
-}
-
-void printLetterb3(bool letter[])
-{
-  for (int Pin = 0; Pin <= 7; Pin++) {
-    if (letter[m3] == 1) {
-      leds3[Pin] = CRGB::Green;
-    }
-    else {
-      leds3[Pin] = CRGB::Black;
-    }
-    m3++;
-  }
-  FastLED.show();
-}
-
-
-// Install Pin change interrupt for a pin, can be called multiple times
-
-/*void pciSetup(byte pin)
-  {
-     digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
-    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
-    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
-  }
-*/
-// Use one Routine to handle each group
-
-void ISR () // handle pin change interrupt for D0 to D7 here
+/*void ISR () // handle pin change interrupt for D0 to D7 here
 {
   if (mybool == 0) {
     State = 1;
     mybool = 1;
   }
   else mybool = 0;
-}
+}*/
